cfc8cd62bf501731016333606a2b7f4de172c1ac
Jennifer Nordquist <nordquist.jennifer@gmail.com>
2017-03-20 14:31:23 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+app.get('/index/html', function (req, res) {
+	res.sendFile(__dirname + "/" + "index.html");
+})
+
+var server = app.listen(8081, function(){
+	var host = server.address().address
+	var port = server.address(). port
+
+	console.log("GM Tools app listening at http://%s:%s", host, port)
+})
